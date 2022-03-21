bccb48047b283d0bd31f6c89a5d5e373b90ce915
Y.Oshima <41306778+y-os@users.noreply.github.com>
2022-03-22 01:04:51 +0900

    Add files via upload

--- page_header.php
+++ page_header.php
@@ -15,7 +15,7 @@
 <meta name="format-detection" content="telephone=no">
 <meta name="robots" content="noindex,nofollow">
 
-<script async src="./Chart.js/Chart.min.js"></script>
+<script async src="./chart.js/chart.min.js"></script>
 
 </head>
 <body>
